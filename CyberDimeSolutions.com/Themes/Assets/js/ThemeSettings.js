8b74c3c5ba48d4ae4ba62f35140a7c3e229f3ab2
Sunil Salaria <sunilsalaria1993@gmail.com>
2022-07-09 01:00:46 +0530

    bootstrap

---
+++ CyberDimeSolutions.com/Themes/Assets/js/ThemeSettings.js
@@ -0,0 +1,11 @@
+﻿/* =================================== */
+/*           Custom js Section
+/* =================================== */
+
+$(document).ready(function () {
+// header section nav items active class add/remove
+    $("header nav ul.navbar-nav li").click(function () {
+        $("header nav ul.navbar-nav li").removeClass("active");
+        $(this).addClass("active");
+    });    
+});
